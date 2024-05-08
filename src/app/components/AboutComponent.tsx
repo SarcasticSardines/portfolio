447cf4ed0d0790db72b8186eb6541ba6299de398
SarcasticeSardines <a.hillstrom311@gmail.com>
2024-05-07 17:53:09 -0700

    apostrophe ruined my deployment

--- src/app/components/AboutComponent.tsx
+++ src/app/components/AboutComponent.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const AboutComponent = () => {
   return (
       <div className='pe-[30%]'>
-          <p>Hello, I'm Avery</p>
+          <p>Hello, I&#8217;m Avery</p>
           <p>I am a junior web developer based in Lodi, California. While new to software concepts, I have over half a year of experience in both front and back end processes. I love to think outside the box, and I take a creative and strategic approach to building and planning projects.</p>
         </div>
   )
